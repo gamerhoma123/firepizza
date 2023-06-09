757c886c16517f71633d0b385c3151613685ad54
gamerhoma123 <gamerhoma123@gmail.com>
2023-06-09 17:10:54 +0545

    day 2-2

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,7 +8,7 @@ export default defineNuxtConfig({
         href:'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css'
       }
        
-      ]
+      ],
       script:[
         {
           src:'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js',
